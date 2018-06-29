0af3cd5b3d607578f75895b071988eaa09fa0439
jagathv <jagathvythee@gmail.com>
2018-06-29 15:46:17 -0400

    added cmdline arg for reference genome to sv_random.sh

--- sv_random.sh
+++ sv_random.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-SVsim -i $1 -r hs37d5.fa -o $2
+SVsim -i $1 -r $3 -o $2
 
 perl fasta_to_fastq.pl $2.fasta > $2.fastq
 
@@ -9,6 +9,6 @@ bowtie2 -x hs_index -U $2.fastq -S $2.sam
 
 samtools view -b -S -o $2.bam $2.sam
 
-samtools mpileup -g -f hs37d5.fa $2.bam > $2.bcf
+samtools mpileup -g -f $3 $2.bam > $2.bcf
 
 bcftools call -c -v $2.bcf > $2.vcf
